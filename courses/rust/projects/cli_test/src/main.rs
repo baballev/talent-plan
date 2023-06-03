7d87df9b782b016a96d7630457851c39b16bbc0e
Vincent Josse <vincent.josse@telecom-paris.fr>
2023-06-03 13:41:44 +0200

    Implementing cat

--- courses/rust/projects/cli_test/src/main.rs
+++ courses/rust/projects/cli_test/src/main.rs
@@ -1,6 +1,20 @@
-use std::env;
+// Small implementation of UNIX's cat to practice CLI programs in rust
+extern crate clap;
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+}
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    println!("{:?}", args)
+    let args = Args::parse();
+    let file = fs::read_to_string(args.path).unwrap();
+    for line in file.lines() {
+        println!("{line}");
+    }
 }
